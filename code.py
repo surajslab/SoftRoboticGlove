5388dedfe3aeb58c06d306596551f84b2664003c
d suraj <dsurajkaku@gmail.com>
2021-10-04 18:36:58 +0530

    Initial Commit

---
+++ code.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+import sklearn
+
+from sklearn.utils import shuffle
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+
+
+
+
+
+
+# reading the data
+df=pd.read_csv( 'dataset.csv')
+print(df)
+
+"""predict="Target"
+x=np.array(df.drop([predict],1))
+y=np.array(df[predict])
+
+
+
+
+
+svm_model=SVC()
+
+#SUPPORT VECTOR MACHINE ALGORITHM
+svm_model.fit(x,y)
+
+
+print(svm_model.predict([1,5,8,1]))
+  """
+   
+  
+    
+   
+
+    
+
+
+
+        
+
+
+
+
+
+
